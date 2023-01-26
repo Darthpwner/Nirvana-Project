d822b5cc13e98cfe2504be7fb410817d524b782a
Matthew Lin <mlin@roblox.com>
2023-01-26 00:29:58 -0800

    first commit

---
+++ api.py
@@ -0,0 +1,22 @@
+import requests
+
+class API:
+	def __init__(self, member_id):
+		self.member_id = member_id
+
+	def call_api(self, base_url):
+		url = "{}?member_id={}".format(base_url, self.member_id)
+		print("url: {}".format(url))
+
+		# Call API and get response
+		response = requests.get(base_url)
+		print(response.status_code)
+		print(response.json())
+		print("\n")
+
+
+
+x = API(1)
+x.call_api("http://127.0.0.1:5000/api1")
+# x.call_api("https://api2.com")
+# x.call_api("https://api3.com")
---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Welcome to this test server!"
+
+@app.route("/api1")
+def api1():
+	return "{deductible: 1000, stop_loss: 10000, oop_max: 5000}"
+
+@app.route("/api2")
+def api1():
+	return "{deductible: 1200, stop_loss: 13000, oop_max: 6000}"
+
+@app.route("/api3")
+def api1():
+	return "{deductible: 1000, stop_loss: 10000, oop_max: 6000}"
