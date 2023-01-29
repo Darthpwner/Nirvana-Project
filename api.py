e98f1b5a7d3ca7f790070e4f9aff93174de77468
Matthew Lin <mlin@roblox.com>
2023-01-29 01:26:17 -0800

    Use aggregate call

--- api.py
+++ api.py
@@ -15,13 +15,17 @@ class API:
 		print("\n")
 
 	def aggregate_apis(self, member_id):
-		pass
+		x.call_api("http://127.0.0.1:5000/api1", member_id)
+		x.call_api("http://127.0.0.1:5000/api2", member_id)
+		x.call_api("http://127.0.0.1:5000/api3", member_id)
 
 
 
 x = API()
-x.call_api("http://127.0.0.1:5000/api1", 1)
-x.call_api("http://127.0.0.1:5000/api2", 1)
-x.call_api("http://127.0.0.1:5000/api3", 1)
+# x.call_api("http://127.0.0.1:5000/api1", 1)
+# x.call_api("http://127.0.0.1:5000/api2", 1)
+# x.call_api("http://127.0.0.1:5000/api3", 1)
 
-x.call_api("http://127.0.0.1:5000/api1", 2)
+# x.call_api("http://127.0.0.1:5000/api1", 2)
+
+x.aggregate_apis(1)
