ac4c2de8017c38ead7f9f0213dedcba92750b89e
Matthew Lin <mlin@roblox.com>
2023-01-29 02:03:53 -0800

    Code cleanup

--- api.py
+++ api.py
@@ -14,18 +14,26 @@ class API:
 		print(response.json())
 		print("\n")
 
-	def aggregate_apis(self, member_id):
-		x.call_api("http://127.0.0.1:5000/api1", member_id)
-		x.call_api("http://127.0.0.1:5000/api2", member_id)
-		x.call_api("http://127.0.0.1:5000/api3", member_id)
+		return response.json()
 
+	def aggregate_apis(self, member_id):
+		result1 = self.call_api("http://127.0.0.1:5000/api1", member_id)
+		result2 = self.call_api("http://127.0.0.1:5000/api2", member_id)
+		result3 = self.call_api("http://127.0.0.1:5000/api3", member_id)
+
+		aggregate_deductible = int((result1["deductible"] + result2["deductible"] + result3["deductible"])/3)
+		aggregate_stop_loss = int((result1["stop_loss"] + result2["stop_loss"] + result3["stop_loss"])/3)
+		aggregate_oop_max = int((result1["oop_max"] + result2["oop_max"] + result3["oop_max"])/3)
+
+		return {
+			"deductible": aggregate_deductible,
+			"stop_loss": aggregate_stop_loss,
+			"oop_max": aggregate_oop_max
+		}
 
 
 x = API()
-# x.call_api("http://127.0.0.1:5000/api1", 1)
-# x.call_api("http://127.0.0.1:5000/api2", 1)
-# x.call_api("http://127.0.0.1:5000/api3", 1)
+print(x.aggregate_apis(1))
+print(x.aggregate_apis(2))
+print(x.aggregate_apis())
 
-# x.call_api("http://127.0.0.1:5000/api1", 2)
-
-x.aggregate_apis(1)
