791bc0e9f92820a22bdc3d3cca5ba70c47e418a2
Matthew Lin <mlin@roblox.com>
2023-01-29 00:36:57 -0800

    Fix bugs

--- server.py
+++ server.py
@@ -11,9 +11,9 @@ def api1():
 	return "{deductible: 1000, stop_loss: 10000, oop_max: 5000}"
 
 @app.route("/api2")
-def api1():
+def api2():
 	return "{deductible: 1200, stop_loss: 13000, oop_max: 6000}"
 
 @app.route("/api3")
-def api1():
+def api3():
 	return "{deductible: 1000, stop_loss: 10000, oop_max: 6000}"
