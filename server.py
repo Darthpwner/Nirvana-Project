1557f6e5c2885122d928a600c2d2e2ff9f7253fb
Matthew Lin <mlin@roblox.com>
2023-01-29 00:44:20 -0800

    Set up JSON correctly

--- server.py
+++ server.py
@@ -8,12 +8,24 @@ def hello_world():
 
 @app.route("/api1")
 def api1():
-	return "{deductible: 1000, stop_loss: 10000, oop_max: 5000}"
+	return {
+        "deductible": 1000,
+        "stop_loss": 10000,
+        "oop_max": 5000
+    }
 
 @app.route("/api2")
 def api2():
-	return "{deductible: 1200, stop_loss: 13000, oop_max: 6000}"
+	return {
+        "deductible": 1200,
+        "stop_loss": 13000,
+        "oop_max": 6000
+    }
 
 @app.route("/api3")
 def api3():
-	return "{deductible: 1000, stop_loss: 10000, oop_max: 6000}"
+	return {
+        "deductible": 1000,
+        "stop_loss": 10000,
+        "oop_max": 6000
+    }
