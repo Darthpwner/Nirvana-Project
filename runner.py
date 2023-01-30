b74661567183d90035b1da440c5554bbadd9ace8
Matthew Lin <mlin@roblox.com>
2023-01-30 09:25:50 -0800

    Use positional args

--- runner.py
+++ runner.py
@@ -1,24 +1,25 @@
 from api import *
 
+def setup_args():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("--member_id", type=str, help="The member_id we want to access.", required=True)
+	args = parser.parse_args()
+
+	return args
+
 def main():
 	coalesce = CoalesceAPI()
 
-	print(coalesce.aggregate_apis(1, mean_config))
-	print("mean_config\n")
-	print(coalesce.aggregate_apis(1, median_config))
-	print("median_config\n")
-	print(coalesce.aggregate_apis(1, min_config))
-	print("min_config\n")
-	print(coalesce.aggregate_apis(1, max_config))
-	print("max_config\n")
+	args = setup_args()
 
-	print(coalesce.aggregate_apis(2, mean_config))
+	print(coalesce.aggregate_apis(args.member_id, mean_config))
 	print("mean_config\n")
-	print(coalesce.aggregate_apis(2, median_config))
+	print(coalesce.aggregate_apis(args.member_id, median_config))
 	print("median_config\n")
-	print(coalesce.aggregate_apis(2, min_config))
+	print(coalesce.aggregate_apis(args.member_id, min_config))
 	print("min_config\n")
-	print(coalesce.aggregate_apis(2, max_config))
+	print(coalesce.aggregate_apis(args.member_id, max_config))
 	print("max_config\n")
 
 if __name__ == "__main__":
