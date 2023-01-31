bc511cc9b58640ed77b64be0b0609d8818fd3c92
Matthew Lin <mlin@roblox.com>
2023-01-30 18:48:04 -0800

    More graceful error handling

--- runner.py
+++ runner.py
@@ -1,32 +1,38 @@
 from api import *
 
 def setup_args():
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument("--member_id", type=str, help="The member_id we want to access.", required=True)
-	args = parser.parse_args()
+    parser.add_argument("--member_id", type=str, help="The member_id we want to access.", required=True)
+    args = parser.parse_args()
 
-	return args
+    return args
 
 def main():
-	coalesce = CoalesceAPI()
-	args = setup_args()
+    coalesce = CoalesceAPI()
+    args = setup_args()
 
-	print("mean_config")
-	print("coalesced result: {}".format(coalesce.aggregate_apis(args.member_id, mean_config)))
-	print("\n")
+    if args.member_id.isnumeric() and int(args.member_id) > 0:
+        pass
+    else:   # input is non-positive or not an integer
+        print("Please enter a valid member_id")
+        return 0
 
-	print("median_config")
-	print("coalesced result: {}".format(coalesce.aggregate_apis(args.member_id, median_config)))
-	print("\n")
+    print("mean_config")
+    print("coalesced result: {}".format(coalesce.aggregate_apis(args.member_id, mean_config)))
+    print("\n")
 
-	print("min_config")
-	print("coalesced result: {}".format(coalesce.aggregate_apis(args.member_id, min_config)))
-	print("\n")
+    print("median_config")
+    print("coalesced result: {}".format(coalesce.aggregate_apis(args.member_id, median_config)))
+    print("\n")
 
-	print("max_config")
-	print("coalesced result: {}".format(coalesce.aggregate_apis(args.member_id, max_config)))
-	print("\n")
+    print("min_config")
+    print("coalesced result: {}".format(coalesce.aggregate_apis(args.member_id, min_config)))
+    print("\n")
+
+    print("max_config")
+    print("coalesced result: {}".format(coalesce.aggregate_apis(args.member_id, max_config)))
+    print("\n")
 
 if __name__ == "__main__":
     main()
